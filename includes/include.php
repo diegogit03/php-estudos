25beaf4341967b58c3230efcb819ee6b2049c61c
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-12-28 11:24:34 -0300

    include

---
+++ includes/include.php
@@ -0,0 +1,8 @@
+<div class="titulo">include</div>
+
+<?php
+echo 'Executei essa linha de arquivo include<br />';
+include('include_arquivo.php');
+
+echo soma(3, 8). '<br />';
+echo 'O conteúdo da variavel é '. $variavel. '.';
--- index.php
+++ index.php
@@ -14,6 +14,16 @@
     <main class="principal">
         <div class="conteudo">
             <nav class="modulos">
+                <div class="modulo roxo-escuro">
+                    <h3>9. Includes</h3>
+                    <ul>
+                        <li>
+                            <a href="exercicio.php?dir=includes&file=include">
+                                include
+                            </a>
+                        </li>
+                    </ul>
+                </div>
                 <div class="modulo azul-escuro">
                     <h3>8. Paradigma O.O.</h3>
                     <ul>

4223c940e6ff2617332f9d297784da64c7869be8
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-12-30 17:40:37 -0300

    try/catch

--- menu.php
+++ menu.php
@@ -1,4 +1,14 @@
 <nav class="modulos">
+    <div class="modulo verde">
+        <h3>11. Tratamento de erro</h3>
+        <ul>
+            <li>
+                <a href="exercicio.php?dir=tratamento_erro&file=try_catch">
+                    Try/Catch
+                </a>
+            </li>
+        </ul>
+    </div>
     <div class="modulo laranja-escuro">
         <h3>10. Includes</h3>
         <ul>

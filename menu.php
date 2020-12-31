acabc97e72a981fc4f52ad8a4cfbf14c6119a262
Diego Henrique de Oliveira Madero <dhom91718@gmail.com>
2020-12-31 11:39:10 -0300

    error_handler

--- menu.php
+++ menu.php
@@ -17,6 +17,11 @@
                     Desafio intdiv
                 </a>
             </li>
+            <li>
+                <a href="exercicio.php?dir=tratamento_erro&file=gerenciador_erros">
+                    Error handler
+                </a>
+            </li>
         </ul>
     </div>
     <div class="modulo laranja-escuro">
